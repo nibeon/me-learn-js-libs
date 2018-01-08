295792b5817aa208eb8006a60da42fa89b7a2549
nibeon <nibeon@gmail.com>
2018-01-08 18:52:22 +0200

    продожаем изучать Redux в связке с React по урокам от Monsterlessons
    Signed-off-by: nibeon <nibeon@gmail.com>

--- react-with-redux/src/App.js
+++ react-with-redux/src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { connect } from 'react-redux';
+
+//import './App.css';
 
 class App extends Component {
+  addTrack() {
+    console.log('addTrack', this.trackInput.value);
+    this.props.onAddTrack(this.trackInput.value);
+    this.trackInput.value ='';
+  }
+
   render() {
+    console.log(this.props.testStore);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input type="text" ref={(input) => { this.trackInput = input }} />
+        <button onClick={this.addTrack.bind(this)}>Add Track</button>
+        <ul>
+          {this.props.testStore.map((track, index) =>
+            <li key={index}>{track}</li>
+          )}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  dispatch => ({
+    onAddTrack: (trackName) => {
+      dispatch({ type: 'ADD_TRACK', payload: trackName });
+    }
+  })
+)(App);
+
+//export default App;
--- react-with-redux/src/index.js
+++ react-with-redux/src/index.js
@@ -1,12 +1,43 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
+const initialState = [
+    'Smells teen like spirit',
+    'Enter Sandman'
+];
+
+function playlist(state = initialState, action) {
+    console.log(action);
+    if(action.type === 'ADD_TRACK') {
+        return [
+            ...state,
+            action.payload
+        ];
+    }
+    return state;
+}
+
+const store = createStore(playlist);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
+
+
+
+
+/*
 import { createStore } from 'redux';
 
 function playlist(state = [], action) {
@@ -41,3 +72,4 @@ addTrackBtn.addEventListener('click', () => {
     //console.log('trackName', trackName);
     store.dispatch({ type: 'ADD_TRACK', payload: trackName });
 })
+*/
